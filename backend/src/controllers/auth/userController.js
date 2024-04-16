554f64a8c4e35f16bd174d8029c60efcb9eb9388
maclinz <tafadzwamucheke@gmail.com>
2024-04-16 23:30:38 +0100

    feat: update user

--- backend/src/controllers/auth/userController.js
+++ backend/src/controllers/auth/userController.js
@@ -128,3 +128,46 @@ export const logoutUser = asyncHandler(async (req, res) => {
 
   res.status(200).json({ message: "User logged out" });
 });
+
+// get user
+export const getUser = asyncHandler(async (req, res) => {
+  // get user details from the token ----> exclude password
+  const user = await User.findById(req.user._id).select("-password");
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    // 404 Not Found
+    res.status(404).json({ message: "User not found" });
+  }
+});
+
+// update user
+export const updateUser = asyncHandler(async (req, res) => {
+  // get user details from the token ----> protect middleware
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    // user properties to update
+    const { name, bio, photo } = req.body;
+    // update user properties
+    user.name = req.body.name || user.name;
+    user.bio = req.body.bio || user.bio;
+    user.photo = req.body.photo || user.photo;
+
+    const updated = await user.save();
+
+    res.status(200).json({
+      _id: updated._id,
+      name: updated.name,
+      email: updated.email,
+      role: updated.role,
+      photo: updated.photo,
+      bio: updated.bio,
+      isVerified: updated.isVerified,
+    });
+  } else {
+    // 404 Not Found
+    res.status(404).json({ message: "User not found" });
+  }
+});
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/auth/userController.js";
+import {
+  getUser,
+  loginUser,
+  logoutUser,
+  registerUser,
+  updateUser,
+} from "../controllers/auth/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/logout", logoutUser);
+router.get("/user", protect, getUser);
+router.patch("/user", protect, updateUser);
 
 export default router;
