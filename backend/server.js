72836e5aa58dff888c03809c2e712fded061c484
maclinz <tafadzwamucheke@gmail.com>
2024-04-17 22:56:52 +0100

    feat: error handler

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import connect from "./src/db/connect.js";
 import cookieParser from "cookie-parser";
 import fs from "node:fs";
+import errorHandler from "./src/helpers/errorhandler.js";
 
 dotenv.config();
 
@@ -21,6 +22,9 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// error handler middleware
+app.use(errorHandler);
 
 //routes
 const routeFiles = fs.readdirSync("./src/routes");
